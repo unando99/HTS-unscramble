3fcc2d441d1756bb8f1b7e4bc1a717bd56a77aa1
unando99 <unando99@gmail.com>
2015-06-18 00:56:00 -0700

    Fixed cin.get

--- source.cpp
+++ source.cpp
@@ -13,29 +13,28 @@ int main()
 	std::cout << "Word unscrambler for Hack This Site Programming Challange 1." << std::endl;
 	std::cout << "Instructions: All files should be placed in program directory. \nWordlist should be named 'wordlist.txt'. \nScrambled wordlist should be named 'scram.txt'." << std::endl;
 	std::cout << "Press 'enter' to continue." << std::endl;
-	//std::cin.getline();
-	//std::cin.ingore();
-	//Open Wordlist
+	std::cin.get();
 	std::fstream wordlist;
 	wordlist.open("wordlist.txt", std::ios::in);
 	if(wordlist.fail())
 	{
 		std::cout << "Wordlist not present. Please provide a proper wordlist and try again." << std::endl;
+		return 0;
 	}
-	else
+	std::cout << "Wordlist accepted. Continuing..." << std::endl;
+	std::fstream scrambledWords;
+	scrambledWords.open("scram.txt", std::ios::in);
+	if(scrambledWords.fail())
 	{
-		std::cout << "Wordlist accepted. Continuing..." << std::endl;
-		std::fstream scrambledWords;
-		scrambledWords.open("scram.txt", std::ios::in);
-		if(scrambledWords.fail())
-		{
-			std::cout << "Scrambled Wordlist not present. Please provide a proper wordlist and try again." << std::endl;
-		}
-		else
-		{
-			std::cout << "Scrambled Wordlist accepted. Continuing..." << std::endl;
-		}
+		std::cout << "Scrambled Wordlist not present. Please provide a proper wordlist and try again." << std::endl;
+		return 0;
 	}
+	std::cout << "Scrambled Wordlist accepted. Continuing..." << std::endl;
+
+
+
+	wordlist.close();
+	scrambledWords.close();
 
 
 	return 0;
